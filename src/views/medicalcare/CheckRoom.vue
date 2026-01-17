0be27c3efbf54e56b9e7c00a422175de65000847
suibaixin <3448929180@qq.com>
2026-01-17 13:25:42 +0800

    选择床位封装

---
+++ src/views/medicalcare/CheckRoom.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!-- <Table ref="Refresh" :columns="columns" :fetch-data="getBloodList" :init-params="searchForm"></Table> -->
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue';
+import Table from '../../components/table.vue'
+
+const tableRef = ref<any>(null);
+
+//刷新页面
+const Refresh = () => {
+    tableRef.value.refresh()
+}
+
+</script>
+
+<style scoped lang='less'></style>
